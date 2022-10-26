13270ef575b87569a228013a12dd995ed797c76f
Alton Jorge <bicagold@gmail.com>
2022-10-26 12:23:38 -0300

    feat: migration user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+
+  resources :dashboards, only: [:index]
 end
